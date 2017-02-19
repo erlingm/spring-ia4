55c84fac155f364560652911cfc03fc9b9046022
Erling Molde <erling.molde@moldesoft.no>
2017-02-19 14:01:36 +0100

    Eksempler på bruk av @AutoConfig.

--- chapter2/src/test/java/soundsystem/CDPlayerTest.java
+++ chapter2/src/test/java/soundsystem/CDPlayerTest.java
@@ -1,11 +1,14 @@
 package soundsystem;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 
 /**
@@ -15,11 +18,23 @@ import static junit.framework.TestCase.assertNotNull;
 @ContextConfiguration(classes = CDPlayerConfig.class)
 public class CDPlayerTest {
 
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+    @Autowired
+    private MediaPlayer player;
+
     @Autowired
     private CompactDisc cd;
 
     @Test
     public void cdShouldNotBeNull() {
         assertNotNull(cd);
+    }
+
+    @Test
+    public void play() {
+        player.play();
+        assertEquals("Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\r\n", log.getLog());
     }
 }
