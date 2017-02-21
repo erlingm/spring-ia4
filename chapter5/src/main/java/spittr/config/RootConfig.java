d909dd8faebeca80ef6c0b18631fe479c822b0d5
Erling Molde <erling.molde@moldesoft.no>
2017-02-21 16:42:17 +0100

    Minimumskonfigurasjon.

---
+++ chapter5/src/main/java/spittr/config/RootConfig.java
@@ -0,0 +1,7 @@
+package spittr.config;
+
+/**
+ * Created by Erling Molde on 21.02.2017.
+ */
+public class RootConfig {
+}
---
+++ chapter5/src/main/java/spittr/config/WebConfig.java
@@ -0,0 +1,12 @@
+package spittr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by Erling Molde on 21.02.2017.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+}
