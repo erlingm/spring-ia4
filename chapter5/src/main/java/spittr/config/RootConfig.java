45f418e1d78d46e94957070c51cfbb497baa0828
Erling Molde <erling.molde@moldesoft.no>
2017-02-21 16:56:19 +0100

    Introduced view resolver, static resource handling, and the non-web RootConfig.

--- chapter5/src/main/java/spittr/config/RootConfig.java
+++ chapter5/src/main/java/spittr/config/RootConfig.java
@@ -1,7 +1,17 @@
 package spittr.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * Created by Erling Molde on 21.02.2017.
  */
+@Configuration
+@ComponentScan(basePackages = {"spitter"}, excludeFilters = {
+        @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
+})
 public class RootConfig {
 }
--- chapter5/src/main/java/spittr/config/WebConfig.java
+++ chapter5/src/main/java/spittr/config/WebConfig.java
@@ -1,12 +1,38 @@
 package spittr.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by Erling Molde on 21.02.2017.
  */
 @Configuration
 @EnableWebMvc
-public class WebConfig {
+@ComponentScan("spitter.web")
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
+
+    /**
+     * This configures Spring's DispatcherServlet to forward requests for static resources
+     * to the servlet container's default servlet and not try to handle them itself.
+     *
+     * @param configurer
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
