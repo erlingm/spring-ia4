f6bedddd31563e258c6fe0c95decfd4cbc071eb1
Erling Molde <erling.molde@moldesoft.no>
2017-02-21 17:27:27 +0100

    Generalized path to home controller.

--- spittr/src/main/java/spitter/web/HomeController.java
+++ spittr/src/main/java/spitter/web/HomeController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by Erling Molde on 21.02.2017.
  */
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String home() {
         return "home";
     }
